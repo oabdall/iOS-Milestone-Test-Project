25596229e7464e0b0752b34815f8993802a08bd2
Omar Abdalla <Omar@Omars-MacBook-Pro-2473.local>
2021-06-10 21:37:34 -0400

    Hello World App

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Omar Abdalla on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
